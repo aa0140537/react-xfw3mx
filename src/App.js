97165ebe49a18e0c1f74be495ef947145486220c
aa0140537 <aa0140537@gmail.com>
2022-01-28 15:41:12 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,80 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 const App = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-    },
-    validate: (values) => {
-      const error = {};
-      if (!values.name) error.name = 'This field is required';
-      return error;
-    },
+  const validationSchema = yup.object({
+    name: yup.string().required('This field is required'),
+    age: yup
+      .number('Please enter a valid age')
+      .required('This field is requirted'),
   });
 
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  // either use useFormik hook or put form in Formik component
+  // const formik = useFormik({
+  //   initialValues: {
+  //     name: '',
+  //     age: '',
+  //   },
+  //   // dont use below validate methos if you are using yup validations
+  //   // validate: (values) => {
+  //   //   const error = {};
+  //   //   if (!values.name) error.name = 'This field is required';
+  //   //   if (!values.age) error.age = 'This field is required';
+  //   //   return error;
+  //   // },
+  //   onSubmit: (values) => {
+  //     console.log(values);
+  //   },
+  //   validationSchema,
+  // });
+
+  const initialValues = {
+    name: ' ',
+    age: '',
+  };
+
   return (
-    <div>
-      <label>Name</label>
-      <input
-        name="name"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      <button type="submit">Submit</button>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <label>Name</label>
+        <Field
+          name="name"
+          // value={formik.values.name}
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // above three lines can be replaces by
+          // {...formik.getFieldProps('name')}
+        />
+        <ErrorMessage name="name" />
+        <br />
+        {/* {formik.touched.name && formik.errors.name && (
+          <p>{formik.errors.name}</p>
+        )} */}
+        <br />
+        <label>Age</label>
+        <Field
+          name="age"
+          // value={formik.values.age}
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // {...formik.getFieldProps('age')}
+        />
+        {/* {formik.touched.age && formik.errors.age && <p>{formik.errors.age}</p>} */}
+        <ErrorMessage name="age" />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
